a8681dd0edb3e1d1b762dda5cde7fe806ff55551
chilikilong112 <chiliki.long@gmail.com>
2022-09-02 17:45:30 +1000

    updating comments in js

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# module-3-challenge
+# <Password Generator>
+
+# URL: https://longpham112.github.io/password-generator/
+
+# Useage:
+Password generator is a site that help users generate password with number of options including lowercase, uppercase, numeral and/or special characters.
+Users need to confirm their choices and the site will provide them with a random password according to their needs.
+
+# Pseudo coding:
+Use prompt to pop up choices:  5 prompts in total
+	• Length 8< characters < 128
+	• Within the chosen number of characters, prop up option to choose character type
+    • Character type: lowercase, uppercase, numeric, and/ or special characters
+
+# Deployed project:
--- script.js
+++ script.js
@@ -1,80 +1,78 @@
-// Assignment Code
+// call the generate button as a variable 
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-// function writePassword() {
-//   var futurePassword = {
-//     lowercase: "abcdefghijklmnopqrstuvwxyz",
-//     uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-//     numeric: "0123456789",
-//     specialCharacter: "!@#$%^&*()"
-//   }
-//   var password = generatePassword(); {
-//     var lowercase = confirm("Would you like to include lowercase characters?");
-//     var uppercase = confirm("Would you like to include uppercase characters?");
-//     var numeric = confirm("Would you like to include numeric characters?");
-//     var specialCharacter = confirm("Would you like to include special characters?");
-//   }
-
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-// }
-
+// define a function that ask a sequense of prompted message so users can choose 
 function writePassword() {
-
-
-
   var characterLength = 0;
+  //run a loop that let users choose a number from 8 to 128
   while (Number.isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
     var characters = prompt("You must choose a password between 8 and 128 characters!", "How many characters would you like?");
+    // turn a string into a number
     characterLength = parseInt(characters);
   }
-
-
+  //a number of confirm box for users
   var lowercase = confirm("Would you like to include lowercase characters?");
   var uppercase = confirm("Would you like to include uppercase characters?");
   var numeric = confirm("Would you like to include numeric characters?");
   var specialCharacter = confirm("Would you like to include special characters?");
-
+  //condition that will stop the function if users do not choose any of the option
   if (!lowercase && !uppercase && !numeric && !specialCharacter) {
-    alert("DCM");
+    alert("Please choose the character(s) for your password!");
     return;
   }
-
-
-  var password = generatePassword({ length: characterLength, lowercase, uppercase, specialCharacter, numeric });
-  console.log(password);
+  // create a var for the generated password
+  var password = generatePassword({
+    length: characterLength,
+    lowercase,
+    uppercase,
+    specialCharacter,
+    numeric
+  });
+  // testing
+  // console.log(password);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
+// define a function to create password
 function generatePassword(options) {
-
+  // numbers of string to choose from
   var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
   var numeric = '0123456789';
   var specialCharacters = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
 
   var password = "";
+  // call a loop to create a string of password according to user's needs
   while (password.length < options.length) {
+    // to randomly choose a character from the given string
     var randomStringIndex = Math.ceil(string.length * Math.random() * Math.random());
     var randomNumbericIndex = Math.ceil(numeric.length * Math.random() * Math.random());
     var randomSpecialCharIndex = Math.ceil(specialCharacters.length * Math.random() * Math.random());
     var hold = "";
-    if (options.lowercase) hold += string.charAt(randomStringIndex);
-    if (options.uppercase) hold += string.charAt(randomStringIndex).toUpperCase();
-    if (options.numeric) hold += numeric.charAt(randomNumbericIndex);
-    if (options.specialCharacter) hold += specialCharacters.charAt(randomSpecialCharIndex);
-
+    // call the random character if users chose the option of lowercase, uppercase, numeric or special characters
+    if (options.lowercase) {
+      hold += string.charAt(randomStringIndex)
+    };
+    if (options.uppercase) {
+      hold += string.charAt(randomStringIndex).toUpperCase()
+    };
+    if (options.numeric) {
+      hold += numeric.charAt(randomNumbericIndex)
+    };
+    if (options.specialCharacter) {
+      hold += specialCharacters.charAt(randomSpecialCharIndex)
+    };
+    // generate a password
     password += hold;
-    if (password.length > options.length) password = password.substring(0, options.length - 1);
+    // if a password is longer than needed, it will cut the password short to fit user's desire
+    if (password.length > options.length) {
+      password = password.substring(0, options.length - 1)
+    };
   }
   return password;
 }
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// var characters = prompt("You must choose a password between 8 and 128 characters!", "How many characters would you like?");
+
 
 
 
